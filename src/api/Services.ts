b406702133bc79858c81758e68db20809285935e
Tony <tonysoto915@gmail.com>
2025-03-11 21:50:59 -0600

    interface correctly displays

--- src/api/Services.ts
+++ src/api/Services.ts
@@ -13,6 +13,72 @@ export const FetchSymbolServices = async (): Promise<string> => {
   
     return (getSymbol);
   };
+  interface MarketData{
+    dailyBar: {
+      close: number,
+      high: number ,
+      low: number ,
+      numberoftrades: number,
+      openPrice: number ,
+      Timestramp: string ,
+      Volume: number,
+      VolumeWeighted: number,
+    },
+    latestQuote: {
+      AskPrice: number,
+      AskSize: number,
+      AskExchange:string,
+      BidPrice: number,
+      BidSize: number,
+      BidExchange: string,
+      Conditions:conditions[],
+      TimeStamp: string,
+      Tape: string,
+    },
+    latestTrade: {
+      Conditions: conditions[],
+      Trade_id: number,
+      Price: number,
+      Size: number,
+      Timestamp: string,
+      Exchange: string,
+      Tape: string,
+    },
+    minuteBar: {
+      ClosePrice: number,
+      HighPrice: number,
+      LowPrice: number,
+      NumberOfTrades: number,
+      OpenPrice: number,
+      Timestamp: string,
+      Volume: number,
+      VolumeWeightedAveragePrice: number,
+    },
+    prevDailyBar: {
+      ClosePrice: number,
+      HighPrice: number,
+      LowPrice: number,
+      NumberOfTrades: number,
+      OpenPrice: number,
+      TimeStamp: string,
+      Volume: number,
+      VolumeWeightedAveragePrice: number,
+    },
+    symbol: string,
+  }
+  interface conditions{
+    condition: string,
+  }
+  export const getMarketData = async (): Promise<MarketData>  => {
+    console.log("you made it here");
+    const response:any = await axios.get(`http://localhost:3000/api/mapped/TSLA/snapshot`);
+    
+    const marketData = response.data;
+    console.log("response",response.data)
+
+    return marketData;
+ 
+  }
 
 
 
--- src/components/stockChart.tsx
+++ src/components/stockChart.tsx
@@ -1,35 +1,99 @@
 import React, { useState,useEffect } from 'react';
-import {FetchSymbolServices} from '../api/Services'
+import {FetchSymbolServices, getMarketData} from '../api/Services'
  
+
+
+interface MarketData{
+    dailyBar: {
+      close: number,
+      high: number ,
+      low: number ,
+      numberoftrades: number,
+      openPrice: number ,
+      Timestramp: string ,
+      Volume: number,
+      VolumeWeighted: number,
+    },
+    latestQuote: {
+      AskPrice: number,
+      AskSize: number,
+      AskExchange:string,
+      BidPrice: number,
+      BidSize: number,
+      BidExchange: string,
+      Conditions:conditions[],
+      TimeStamp: string,
+      Tape: string,
+    },
+    latestTrade: {
+      Conditions: conditions[],
+      Trade_id: number,
+      Price: number,
+      Size: number,
+      Timestamp: string,
+      Exchange: string,
+      Tape: string,
+    },
+    minuteBar: {
+      ClosePrice: number,
+      HighPrice: number,
+      LowPrice: number,
+      NumberOfTrades: number,
+      OpenPrice: number,
+      Timestamp: string,
+      Volume: number,
+      VolumeWeightedAveragePrice: number,
+    },
+    prevDailyBar: {
+      ClosePrice: number,
+      HighPrice: number,
+      LowPrice: number,
+      NumberOfTrades: number,
+      OpenPrice: number,
+      TimeStamp: string,
+      Volume: number,
+      VolumeWeightedAveragePrice: number,
+    },
+    symbol: string,
+  }
+  interface conditions{
+    condition: string,
+  }
 const StockChart: React.FC = () => {
 
-    const [stockData, setStockData] = useState<any>(null);
-    const [symbol, setSymbol] = useState<string | null>(null);
+    const [stockData, setStockData] = useState<MarketData | null>();
+    const [symbol, setSymbol] = useState<string| null >();
 
-    const  SymbolTest = async() => {
-    const fetchedSymbol = await FetchSymbolServices();
-    setSymbol(fetchedSymbol);
-    setStockData(fetchedSymbol)
-    console.log('symbol', Symbol);
-    }
+    const  DataTest = async() => {
+
+        const marketData:MarketData = await getMarketData();
+       
+        setStockData(marketData);
+        setSymbol(marketData.symbol)
+    
+    
+     console.log('fetchedSymbol', marketData);
+        }
 
 
 
     return (
         <div className="stock-chart-watchlist">
             <div className="watchlist">
-                <button onClick={() => SymbolTest()}>Fetch Symbol</button>
+                <button onClick={() => DataTest()}>Fetch Symbol</button>
             </div>
             <div className="stock-chart">
-                <h2>Selected Symbol: {symbol ? symbol : 'No symbol selected'}</h2> {/* Display symbol */}
+                <h2>Selected Symbol: {symbol ? symbol : 'No symbol selected'}</h2> 
                 {stockData ? (
                     <div>
                         <p>Stock Info: {JSON.stringify(stockData, null, 2)}</p>
-                        <div>Chart for {symbol} goes here</div>
+                      
                     </div>
                 ) : (
                     <p>Loading stock data...</p>
                 )}
+
+                
             </div>
         </div>
     );
